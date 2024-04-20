48157cf22155fe699e3e99e579ba4eb9ad1ed12d
foykes <foykes@gmail.com>
2024-04-20 23:54:34 +0300

    errors fix

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ current_path = os.getcwd()
 os.system(('jupyter nbconvert --to script {}/data_gathering.ipynb').format(current_path))
 os.system(('jupyter nbconvert --to script {}/tech.ipynb').format(current_path))
 
-os.system(('python {}/data_gathering.py')).format(current_path)
-os.system(('python {}/tech.py')).format(current_path)
+os.system(('python {}/data_gathering.py').format(current_path))
+os.system(('python {}/tech.py').format(current_path))
